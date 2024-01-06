683667150c2604d25a59fee836e09d44b2d29252
Wsei37645 <100755837+Wsei37645@users.noreply.github.com>
2024-01-06 15:56:11 +0100

    Add files via upload

--- js/toTop.js
+++ js/toTop.js
@@ -2,3 +2,12 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+function responsive(){
+    var rspMenu = document.getElementsByTagName("menu")[0];
+    if(rspMenu.style.display === "none"){
+        rspMenu.style.display = "block";
+    }else{
+        rspMenu.style.display = "none";
+    }
+                
+}
